4b4bdfaa6fb09d24572d0351bbf0cdb30fb82037
Nakovanj <nakovanj@gmail.com>
2023-11-26 22:54:54 +0100

    [weather-app] Fix port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const hbs = require('hbs')
 
-const port = proces.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 // forecast functions
 const geocode = require('./utils/geocode')
